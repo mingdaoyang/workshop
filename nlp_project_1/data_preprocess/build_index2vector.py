cf293a79c91b847e7f6fc4c8cac1c0650383740c
’mingdaoyang <mingdaoyang@qq.com>
2020-04-30 14:56:25 +0800

    update

---
+++ nlp_project_1/data_preprocess/build_index2vector.py
@@ -0,0 +1,29 @@
+from gensim.models import KeyedVectors
+import json
+from collections import OrderedDict
+from annoy import AnnoyIndex
+from annoy import AnnoyIndex
+def built_vocab(input_modle_path):
+    #加载二进制模型
+    wv_model = KeyedVectors.load_word2vec_format(input_modle_path, binary=True)
+    word_index = OrderedDict() #有序字典
+    #给wv_model.vovab中的词加index
+    for counter, key in enumerate(wv_model.vocab.keys()):
+        word_index[key] = counter
+    #写入json文件
+    with open('../data/word_index.json', 'w') as fp:
+        json.dump(word_index, fp)
+    #通过Annoy算法建立索引树，256为词向量长度
+    wv_index = AnnoyIndex(256,metric='angular')
+    i = 0
+    #针对wv_index中的每一个index i 写入相应的词向量,和原始dic的index不同
+    for word in wv_model.vocab.keys():
+        v = wv_model[word]
+        wv_index.add_item(i, v)
+        i += 1
+    wv_index.build(10) #10 trees
+    wv_index.save('../data/wv_index.index')
+
+if __name__=='__main__':
+    path = 'w2v.bin'
+    built_vocab(path)
---
+++ nlp_project_1/data_preprocess/build_vocab.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+def save_word_dict(vocab, save_path):
+    with open(save_path, 'w', encoding='utf-8') as f:
+        for line in vocab:
+            w, i = line
+            f.write("%s\t%d\n" % (w, i))
+
+
+def read_data(path_1, path_2, path_3):
+    with open(path_1, 'r', encoding='utf-8') as f1, \
+            open(path_2, 'r', encoding='utf-8') as f2, \
+            open(path_3, 'r', encoding='utf-8') as f3:
+        words = []
+        for line in f1:
+            words = line.split()
+
+        for line in f2:
+            words += line.split(' ')
+
+        for line in f3:
+            words += line.split(' ')
+
+    return words
+
+
+def build_vocab(items, sort=True, min_count=0, lower=False):
+    """
+    构建词典列表
+    :param items: list  [item1, item2, ... ]
+    :param sort: 是否按频率排序，否则按items排序
+    :param min_count: 词典最小频次
+    :param lower: 是否小写
+    :return: list: word set
+    """
+    result = []
+    if sort:
+        # sort by count
+        dic = defaultdict(int) #先生成一个空字典dic，当dic的key不存在时，返回的是工厂函数的默认值，int为0
+        for item in items:
+            for word in item.split(" "): #使用i很诡异,这里改成word
+                word = word.strip()
+                if not word: continue #如果word不存在，跳过下两行操作
+                word = word if not lower else item.lower()
+                dic[word] += 1 #将并统计词频，将word用dic存储，
+        # sort
+        dic = sorted(dic.items(), key=lambda d: d[1], reverse=True)
+        for word, item in enumerate(dic):
+            key = item[0]
+            if min_count and min_count > item[1]: #当min_count为0时，这个item没出现过，因此跳过下面的操作
+                continue
+            result.append(key)
+    else:
+        # sort by items
+        for word, item in enumerate(items):
+            item = item if not lower else item.lower()
+            result.append(item)
+
+    vocab = [(w, i) for i, w in enumerate(result)] #这里的i为经排序枚举后的key
+    reverse_vocab = [(i, w) for i, w in enumerate(result)]
+
+    return vocab, reverse_vocab
+
+if __name__ == '__main__':
+    lines = read_data('../data/train_x.txt',
+                      '../data/train_y.txt',
+                      '../data/test_x.txt')
+    vocab, reverse_vocab = build_vocab(lines)
+    save_word_dict(vocab, '../data/vocab.txt')
